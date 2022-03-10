48f49b66b599846c173d11291fb4ac7a437f5ab8
sreehari2003 <sreeharivijaya2003@gmail.com>
2022-03-11 00:17:55 +0530

    Added advanced protected routing

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,7 @@
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   res.status(err.statusCode).json({
+    ok: false,
     status: err.status,
     message: err.message
   });
